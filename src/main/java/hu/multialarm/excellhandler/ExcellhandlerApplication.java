68e1528b0bca62d38baf2dc8105491b607319ff4
grof.andras <grof.andras@multialarm.com>
2025-01-07 14:07:11 +0100

    Starter empty file structure with dependencies: -JPA -WEB -POI -H2 -TEST -LOMBOK

---
+++ src/main/java/hu/multialarm/excellhandler/ExcellhandlerApplication.java
@@ -0,0 +1,13 @@
+package hu.multialarm.excellhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcellhandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcellhandlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/multialarm/excellhandler/ExcellhandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.multialarm.excellhandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcellhandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
